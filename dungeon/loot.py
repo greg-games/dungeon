dd62367e67b9478d30c0a6947455534e994fd24f
Grześ Mikulski <grzegorz.mikulski@outlook.com>
2024-10-06 15:56:55 +0200

    faster loot animation in browser

--- dungeon/loot.py
+++ dungeon/loot.py
@@ -1,4 +1,5 @@
 from pgzero.actor import Actor
+from global_functions import is_in_browser
 
 class Loot(Actor):
     def __init__(self,name,pos):
@@ -6,7 +7,9 @@ class Loot(Actor):
         super().__init__(f"loot/{self.name}",pos)
         self._orig_surf.set_alpha(0)  
         self.raise_height = 100
-        self.step = self.raise_height/20
+        no_frames = 20
+        if is_in_browser(): no_frames = 5
+        self.step = self.raise_height/no_frames
         self.end_y = self.y - self.raise_height
 
     def is_animating_finished(self): 
