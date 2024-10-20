6908d153eb4ea65a147820b1e1da89e3580e15ab
Grześ Mikulski <grzegorz.mikulski@outlook.com>
2024-10-20 22:08:18 +0200

    override inject_global_handlers

--- dungeon/main.py
+++ dungeon/main.py
@@ -708,13 +708,38 @@ def update():
 
 from pgzrun import run_mod
 from pgzero.game import PGZeroGame
+import sys
 
 pgzero_game = None
 
+def new_inject_global_handlers(self):
+    self.handlers[pygame.QUIT] = lambda e: sys.exit(0)
+    self.handlers[pygame.VIDEOEXPOSE] = lambda e: None
+
+    user_key_down = self.handlers.get(pygame.KEYDOWN)
+    user_key_up = self.handlers.get(pygame.KEYUP)
+
+    def key_down(event):
+        if event.key == pygame.K_q and \
+                event.mod & (pygame.KMOD_CTRL | pygame.KMOD_META):
+            sys.exit(0)
+        self.keyboard._press(event.key)
+        if user_key_down:
+            return user_key_down(event)
+
+    def key_up(event):
+        self.keyboard._release(event.key)
+        if user_key_up:
+            return user_key_up(event)
+
+    self.handlers[pygame.KEYDOWN] = key_down
+    self.handlers[pygame.KEYUP] = key_up
+    self.handlers[pygame.FINGERDOWN] = on_finger_down
+    self.handlers[pygame.FINGERUP] = on_finger_up
+
 def my_run_mod(mod, **kwargs):
     pgzero_game = PGZeroGame(mod, **kwargs)
-    pgzero_game.handlers[pygame.FINGERDOWN] = on_finger_down
-    pgzero_game.handlers[pygame.FINGERUP] = on_finger_up
+    pgzero_game.inject_global_handlers = new_inject_global_handlers.__get__(pgzero_game, PGZeroGame)
     pgzero_game.run()
 
 pgzrun.run_mod = my_run_mod
